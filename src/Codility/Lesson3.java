ea5f54aa27b89569abd7298570796f08088b0c4e
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2018-09-09 15:48:17 +0300

    Codility lesson 3 55%(100) 8 min + 8 min (FIXED)

--- src/Codility/Lesson3.java
+++ src/Codility/Lesson3.java
@@ -4,15 +4,16 @@ import java.util.Arrays;
 
 public class Lesson3 {
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{3, 4, 2, 4,3, 5, 6, 6, 8, 7, 5, 7, 8}));
+        System.out.println(solution(new int[]{3, 4, 2, 4, 2, 9, 3, 5, 6, 6, 8, 5, 7, 7, 8}));
     }
-    public static int solution(int[] A) {
+
+    private static int solution(int[] A) {
         Arrays.sort(A);
-        for (int i = 0; i <A.length -1 ; i = i+2) {
-            if (A[i]!= A[i+1]){
+        for (int i = 0; i < A.length - 1; i = i + 2) {
+            if (A[i] != A[i + 1]) {
                 return A[i];
             }
         }
-        return 0;
+        return A[A.length - 1];
     }
 }
