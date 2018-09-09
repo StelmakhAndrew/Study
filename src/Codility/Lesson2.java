941b4f1e16cd6031a1e6208fa94b5af5a316a9da
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2018-09-09 14:18:51 +0300

    Codility lesson 2 12% 30min))))

--- src/Codility/Lesson2.java
+++ src/Codility/Lesson2.java
@@ -9,15 +9,14 @@ public class Lesson2 {
     }
     public static int[] solution(int[] A, int K) {
         K = K % A.length;
-        System.out.println(K);
+
+        if (K == 0)return A;
         int [] resualt1;
         int [] resualt2;
         int [] resualt;
 
         resualt1 = Arrays.copyOfRange(A,A.length - K  , A.length);
-        System.out.println(Arrays.toString(resualt1));
         resualt2 = Arrays.copyOfRange(A , 0, K -1  );
-        System.out.println(Arrays.toString(resualt2));
         resualt = Arrays.copyOf(resualt1 , A.length);
         System.arraycopy(resualt2, 0, resualt, A.length - K+1, K-1);
 
