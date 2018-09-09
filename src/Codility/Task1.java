120c6fa861aaaf8a5c44b2802a558c5d23ccc5a7
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2018-09-09 13:11:42 +0300

    Codility lesson 1 80 (100)

--- src/Codility/Task1.java
+++ src/Codility/Task1.java
@@ -2,17 +2,37 @@ package Codility;
 
 public class Task1 {
     public static void main(String[] args) {
-    solution(5);
+        System.out.println(solution(5));
+        System.out.println(solution(529));
+        System.out.println(solution(32));
+        System.out.println(solution(1041));
     }
     public static int solution(int N) {
         String b ="";
         while(N !=0 ) {
-            b =  b + N%2;
+            b = b +(N % 2);
             N = N/2;
         }
         System.out.println(b);
-
-
-        return N;
+        char [] ss = b.toCharArray();
+        int maxLen= 0 ;
+        int count = 0;
+        boolean flag = false;
+        for (int i = 0; i < ss.length ; i++) {
+            if (ss[i] == '1'){
+                flag =true;
+            }
+            if (ss[i] == '0' && flag )
+            {
+                count++;
+            } else if (ss[i] == '1')
+            {
+                if (count> maxLen) {
+                    maxLen = count;
+                    count = 0;
+                }
+            }
+        }
+        return maxLen;
     }
 }
