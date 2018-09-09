6d05a28607fe7d1df936bbac4a288901132374c4
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2018-09-09 14:44:13 +0300

    Codility lesson 2 12(87)% 30min + 20 min+2 min (FIXED)

--- src/Codility/Lesson2.java
+++ src/Codility/Lesson2.java
@@ -7,23 +7,25 @@ public class Lesson2 {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(solution(new int[]{}, 1)));
     }
+
     public static int[] solution(int[] A, int K) {
-        if (A.length ==0)
+        int len = A.length;
+        if (len == 0)
             return A;
 
-        K = K % A.length;
+        K = K % len;
 
-        if (K == 0)return A;
-        int [] resualt1;
-        int [] resualt2;
-        int [] resualt;
+        if (K == 0) return A;
+        int[] resualt1;
+        int[] resualt2;
+        int[] resualt;
 
-        resualt1 = Arrays.copyOfRange(A,A.length - K  , A.length);
+        resualt1 = Arrays.copyOfRange(A, len - K, len);
         System.out.println(Arrays.toString(resualt1));
-        resualt2 = Arrays.copyOfRange(A , 0, A.length - K  );
+        resualt2 = Arrays.copyOfRange(A, 0, len - K);
         System.out.println(Arrays.toString(resualt2));
 
-        resualt = Arrays.copyOf(resualt1 , A.length);
+        resualt = Arrays.copyOf(resualt1, len);
         System.arraycopy(resualt2, 0, resualt, resualt1.length, resualt2.length);
 
         return resualt;
