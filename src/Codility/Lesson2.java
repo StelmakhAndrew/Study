44c7a2e46ed7058fb250a81cf4a31b20cb02a44f
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2018-09-09 14:37:28 +0300

    Codility lesson 2 12(87)% 30min + 20 min))))

--- src/Codility/Lesson2.java
+++ src/Codility/Lesson2.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Lesson2 {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new int[]{3, 8,  9,7, 6}, 3)));
+        System.out.println(Arrays.toString(solution(new int[]{3, 8,  9}, 1)));
     }
     public static int[] solution(int[] A, int K) {
         K = K % A.length;
@@ -16,9 +16,12 @@ public class Lesson2 {
         int [] resualt;
 
         resualt1 = Arrays.copyOfRange(A,A.length - K  , A.length);
-        resualt2 = Arrays.copyOfRange(A , 0, K -1  );
+        System.out.println(Arrays.toString(resualt1));
+        resualt2 = Arrays.copyOfRange(A , 0, A.length - K  );
+        System.out.println(Arrays.toString(resualt2));
+
         resualt = Arrays.copyOf(resualt1 , A.length);
-        System.arraycopy(resualt2, 0, resualt, A.length - K+1, K-1);
+        System.arraycopy(resualt2, 0, resualt, resualt1.length, resualt2.length);
 
         return resualt;
     }
