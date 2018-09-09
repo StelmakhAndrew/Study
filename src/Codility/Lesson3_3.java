506b9943788bbe34cdaff4634f0678a604e58831
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2018-09-09 17:55:22 +0300

    Codility lesson 3_3 83% 19 min + 15 min (TIME NORMAL)

--- src/Codility/Lesson3_3.java
+++ src/Codility/Lesson3_3.java
@@ -7,7 +7,6 @@ public class Lesson3_3 {
 
 
     public static int solution(int[] A) {
-        int k =0;
         int sum1=0;
         int sum2=0;
         int minDif = Integer.MAX_VALUE;
@@ -15,23 +14,14 @@ public class Lesson3_3 {
         for (int elem:A ) {
             sumAr = elem + sumAr;
         }
-        do{
-
-
-            for (int j = 0; j <=k ; j++) {
+            for (int j = 0; j <=A.length-1 ; j++) {
                 sum1 = sum1+A[j];
+                sum2 = sumAr - sum1;
+                if(Math.abs(sum2 - sum1) < minDif){
+                    minDif = Math.abs(sum2 - sum1);
+                }
+
             }
-
-            sum2 = sumAr - sum1;
-
-
-            if(Math.abs(sum2 - sum1) < minDif){
-                minDif = Math.abs(sum2 - sum1);
-            }
-            sum1 =0;
-
-            k++;
-        }while (k!= A.length-1);
         return minDif;
     }
 }
