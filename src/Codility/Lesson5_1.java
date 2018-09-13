67384f87b991853f2027468af2911ed0a8f4a757
StelmakhAndrew <42778039+StelmakhAndrew@users.noreply.github.com>
2018-09-13 17:04:29 +0300

    Lesson5_1 70% 9min + 5min (FIXED)

--- src/Codility/Lesson5_1.java
+++ src/Codility/Lesson5_1.java
@@ -2,17 +2,18 @@ package Codility;
 
 public class Lesson5_1 {
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{0, 1}));
+        System.out.println(solution(new int[]{1, 1,1,1,1,1, 1,1,1,1}));
     }
 
 
     public static int solution(int[] A) {
     int count = 0;
-    int resualt = 0;
+    long result = 0;
         for (int aA : A) {
             if (aA == 0) count++;
-            else resualt = resualt + count;
+            else result = result + count;
         }
-        return resualt;
+        if (result > 1000000000L) return -1;
+        return (int)result;
     }
 }
